89f762f87fa3cf23c48c9e3cfca160d1e52e1089
justtoconfirm <office@justtoconfirm.co.uk>
2022-05-27 11:20:14 +0100

    Add styled-components to the Button component to add initial CSS styling

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,18 @@
 import React, { FC } from 'react';
+import styled from 'styled-components';
 
 interface ButtonProps {
 	children: string
 }
 
+const StyledButton = styled.button<ButtonProps>`
+	display: inline-block;
+`;
+
 const Button: FC<ButtonProps> = ({ children }) => {
 
 	return (
-		<button>{children}</button>
+		<StyledButton>{children}</StyledButton>
 	)
 }
 
