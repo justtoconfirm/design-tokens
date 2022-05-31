3c8429bc0bc57d1cdccbfa394c0b65215bcbe2a4
justtoconfirm <office@justtoconfirm.co.uk>
2022-05-31 14:05:45 +0100

    Further revisions to the Button story in Storybook

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -15,24 +15,40 @@ const Template: ComponentStory<typeof Button> = args => <Button {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
 	theme: 'primary',
+	disabled: false,
+	children: 'Submit',
 }
 
 export const Secondary = Template.bind({});
 Secondary.args = {
 	theme: 'secondary',
+	disabled: false,
+	children: 'Submit',
+}
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+	disabled: true,
+	children: 'Submit',
 }
 
 export const Small = Template.bind({});
 Small.args = {
 	size: 'small',
+	disabled: false,
+	children: 'Submit',
 }
 
 export const Medium = Template.bind({});
 Medium.args = {
 	size: 'medium',
+	disabled: false,
+	children: 'Submit',
 }
 
 export const Large = Template.bind({});
 Large.args = {
 	size: 'large',
+	disabled: false,
+	children: 'Submit',
 }
