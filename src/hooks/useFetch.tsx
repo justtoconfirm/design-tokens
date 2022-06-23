76db5d78b3456d01bfbc6094f549b0908a5e6602
justtoconfirm <office@justtoconfirm.co.uk>
2022-06-23 17:14:33 +0100

    Catch added for error checking in Fetch API hook

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -8,8 +8,11 @@ const useFetch = (url) => {
 		fetch(url, {
 			headers: { 'X-Figma-Token': 'figd_hh8SuP5PHIP_r9yHXC6XXPWv0ttv4eaYqoWBvJjd'}
 		})
-			.then((res) => res.json())
-			.then((data) => setData(data));
+		.then((response) => response.json())
+		.then((data) => setData(data))
+		.catch((err) => {
+			console.log(err.message);
+		});
 	}, [url]);
 
 	return [data];	
